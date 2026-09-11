3425b18da9267fc62b82e36ef7155a1b74dfaf00
chaves99 <viniciusbaleia1999@gmail.com>
2026-09-11 16:37:12 -0300

    feat: update cors.

--- src/main/java/cnpj/analyzr/config/CorsConfig.java
+++ src/main/java/cnpj/analyzr/config/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200") // Replace with your frontend URL
+                .allowedOrigins("http://localhost:4200", "https://lead-mail-manager-angular-production.up.railway.app/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
