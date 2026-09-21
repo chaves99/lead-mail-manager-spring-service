77b3ffaddc0a21066449cb8b09e2b4dc1d275b38
chaves99 <viniciusbaleia1999@gmail.com>
2026-09-21 16:57:29 -0300

    feat: creating aws sns controller.

---
+++ src/main/java/cnpj/analyzr/controller/AwsEmailNotificationController.java
@@ -0,0 +1,29 @@
+package cnpj.analyzr.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("aws-ses-notification")
+public class AwsEmailNotificationController {
+
+    @PostMapping
+    public ResponseEntity<?> post(@RequestBody String body) {
+        log.info("post - body:{}", body);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<?> put(@RequestBody String body) {
+        log.info("put - body:{}", body);
+        return ResponseEntity.ok().build();
+    }
+}
