e95a788a74a4812f386d67d62c051c20db88674d
chaves99 <viniciusbaleia1999@gmail.com>
2026-09-24 15:25:43 -0300

    feat: add spring clound sns lib.

--- src/main/java/cnpj/analyzr/controller/AwsEmailNotificationController.java
+++ src/main/java/cnpj/analyzr/controller/AwsEmailNotificationController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.awspring.cloud.sns.annotation.endpoint.NotificationMessageMapping;
+import io.awspring.cloud.sns.annotation.handlers.NotificationMessage;
+import io.awspring.cloud.sns.annotation.handlers.NotificationSubject;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -24,14 +27,18 @@ public class AwsEmailNotificationController {
     }
 
     @PostMapping("/bounce")
-    public ResponseEntity<?> bounce(@RequestHeader HttpHeaders headers, @RequestBody String body) {
+    public ResponseEntity<?> bounce(@RequestHeader HttpHeaders headers, @RequestBody String body,
+            @NotificationSubject String subject, @NotificationMessage String message) {
         log.info("POST bounce - headers:{} body:{}", headers.toString(), body);
         return ResponseEntity.ok().build();
     }
 
     @PostMapping("/delivered")
-    public ResponseEntity<?> delivered(@RequestHeader HttpHeaders headers, @RequestBody String body) {
+    @NotificationMessageMapping
+    public ResponseEntity<?> delivered(@RequestHeader HttpHeaders headers, @RequestBody String body,
+            @NotificationSubject String subject, @NotificationMessage String message) {
         log.info("POST delivered - headers:{} body:{}", headers.toString(), body);
+        log.info("POST delivered - subject:{} message:{}", subject, message);
         return ResponseEntity.ok().build();
     }
 }
