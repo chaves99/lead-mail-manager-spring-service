1b000557703fd2699c0be3669f5f61460a6c3b55
chaves99 <viniciusbaleia1999@gmail.com>
2026-09-24 09:29:09 -0300

    feat: updating aws notification controller.

--- src/main/java/cnpj/analyzr/controller/AwsEmailNotificationController.java
+++ src/main/java/cnpj/analyzr/controller/AwsEmailNotificationController.java
@@ -2,7 +2,6 @@ package cnpj.analyzr.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,16 +12,16 @@ import lombok.extern.slf4j.Slf4j;
 @RestController
 @Slf4j
 @RequiredArgsConstructor
-@RequestMapping("aws-ses-notification")
+@RequestMapping("/aws-ses-notification")
 public class AwsEmailNotificationController {
 
-    @PostMapping
+    @PostMapping("/complaint")
     public ResponseEntity<?> post(@RequestBody String body) {
         log.info("post - body:{}", body);
         return ResponseEntity.ok().build();
     }
 
-    @PutMapping
+    @PostMapping("/bounce")
     public ResponseEntity<?> put(@RequestBody String body) {
         log.info("put - body:{}", body);
         return ResponseEntity.ok().build();
